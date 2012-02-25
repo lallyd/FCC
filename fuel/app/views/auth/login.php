7facc72986e1133f4ad37721d49a77e8180bad85
Michael Heap <m@michaelaheap.com>
2012-02-25 17:21:14 +0000

    Finish login

--- fuel/app/views/auth/login.php
+++ fuel/app/views/auth/login.php
@@ -1,7 +1,7 @@
-<form class="well form-inline">
+<form class="well form-inline text-center">
 	<div class="container">
 		<input type="text" class="input-small" placeholder="Email">
 		<input type="password" class="input-small" placeholder="Password">
-		<button type="submit" class="btn">Go</button>
+		<button type="submit" class="btn btn-inverse">Go</button>
 	</div>
 </form>
--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="/assets/css/base.css" />
     <link rel="stylesheet" href="/assets/css/bootstrap.css" />
 </head>
 <body>
