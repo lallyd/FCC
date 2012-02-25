54ff6b5d0972c5d864741bb674b96ebac6eb6962
Michael Heap <m@michaelaheap.com>
2012-02-25 18:45:05 +0000

    Add and edit job working

--- fuel/app/views/job/add.php
+++ fuel/app/views/job/add.php
@@ -1,1 +1,47 @@
-<p>Add</p>
+<div class="container">
+    <form class="form-horizontal">
+	    <fieldset>
+	   	 <legend>Add Job</legend>
+		    <div class="control-group">
+			    <label class="control-label" for="name">Status</label>
+			    <div class="controls">
+				    <select class="span4">
+		                <option>1</option>
+		                <option>2</option>
+		                <option>3</option>
+		                <option>4</option>
+		                <option>5</option>
+		            </select>
+			    </div>
+		    </div>
+		    <div class="control-group">
+			    <label class="control-label" for="contact-number">Customer</label>
+			    <div class="controls">
+				   <input type="text" class="span4" id="contact-number" name="contact_number">
+			    </div>
+		    </div>
+		    <div class="control-group">
+			    <label class="control-label" for="contact-number">Fault Description</label>
+			    <div class="controls">
+				    <textarea class="span4" id="contact-number" name="contact_number"></textarea>
+			    </div>
+		    </div>
+		    <div class="control-group">
+			    <label class="control-label" for="contact-number">Shop</label>
+			    <div class="controls">
+				    <select class="span4">
+		                <option>1</option>
+		                <option>2</option>
+		                <option>3</option>
+		                <option>4</option>
+		                <option>5</option>
+		            </select>
+			    </div>
+		    </div>
+		    <div class="form-actions">
+	            <input class="btn btn-success" type="submit" value="Save" />
+	            <a href="<?php echo Uri::Create("job"); ?>" class="btn btn-danger">Cancel</a>
+          </div>
+	    </fieldset>
+    </form>
+</div>
