ef996dd0d8bcf24490a70024db1ba19afdcbf845
Michael Heap <m@michaelaheap.com>
2012-02-26 12:09:40 +0000

    Add login protection to base_private

--- fuel/app/classes/base/private.php
+++ fuel/app/classes/base/private.php
@@ -5,5 +5,11 @@ class Base_Private extends Base_Public
 	public function __construct($req, $res)
 	{
 		parent::__construct($req, $res);
+
+		if (!$this->user->logged_in())
+		{
+			Message::Error("You need to be logged in");
+			Response::Redirect("auth/login");
+		}
 	}
 }
