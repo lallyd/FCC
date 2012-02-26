8315a1f256f96972551707d3c76549e667ba4b04
Michael Heap <m@michaelaheap.com>
2012-02-26 13:38:29 +0000

    Shop add_edit working

--- fuel/app/classes/controller/shop.php
+++ fuel/app/classes/controller/shop.php
@@ -12,17 +12,43 @@ class Controller_Shop extends Base_Private
 	public function action_add()
 	{
 		$this->template->title = 'Add Shop';
+		$shop = new Model_Shop;
+		$this->data['action'] = 'Add';
+		$this->add_edit($shop);
+		
 	}
 
-	public function action_edit()
+	public function action_edit($id)
 	{
+		$shop = Model_Shop::find($id);
 		$this->template->title = 'Edit Shop';
+		$this->data['action'] = 'Edit';
+		$this->add_edit($shop);
+
+
 	}
 
 	public function action_view($id)
 	{
 		$this->data['shop'] = Model_Shop::find($id);
 		$this->template->title = 'View Shop';
+
+	}
+
+	protected function add_edit($m)
+	{
+		if (Input::is_post()){
+		$m->location=Input::post('location');
+		$m->contact_number=Input::post('contact_number');
+		$m->email=Input::post('email');
+		$m->skype=Input::post('skype');
+		$m->save();
+
+		Message::Success("Shop " .$this->data['action']."ed");
+		Response::Redirect("shop/view/".$m->id);			
+		}
+
+		$this->data['shop'] = $m;
 
 	}
 
--- fuel/app/views/shop/addedit.php
+++ fuel/app/views/shop/addedit.php
@@ -1,1 +1,35 @@
-<p>Edit</p>
+<div class="container">
+    <form id="customer-form" class="form-horizontal" method="post" action="">
+	    <fieldset>
+	   	 <legend><?php echo $action; ?> Shop</legend>
+	   	 	<div class="control-group">
+			    <label class="control-label" for="location">Location</label>
+			    <div class="controls">
+				   <input type="text" class="span4" id="location" name="location" value="<?php echo $shop->location; ?>">
+			    </div>
+		    </div>
+		    <div class="control-group">
+			    <label class="control-label" for="email">Email</label>
+			    <div class="controls">
+				   <input type="text" class="span4" id="email" name="email" value="<?php echo $shop->email; ?>">
+			    </div>
+		    </div>
+		    <div class="control-group">
+			    <label class="control-label" for="contact-number">Contact Number</label>
+			    <div class="controls">
+				   <input type="text" class="span4" id="contact-number" name="contact_number" value="<?php echo $shop->contact_number; ?>">
+			    </div>
+		    </div>
+		    <div class="control-group">
+			    <label class="control-label" for="skype">Skype</label>
+			    <div class="controls">
+				   <input type="text" class="span4" id="skype" name="skype" value="<?php echo $shop->skype; ?>">
+			    </div>
+		    </div>
+		    <div class="form-actions">
+	            <input class="btn btn-success" type="submit" value="Save" />
+	            <a href="<?php echo Uri::Create("shop"); ?>" class="btn btn-danger">Cancel</a>
+          </div>
+	    </fieldset>
+    </form>
+</div>
